1d387e8148efa7099c13250f3e3623db78a9eddb
Lars Petter Hauge <larsenhauge@gmail.com>
2025-11-20 14:48:29 +0100

    Add segment lookup table for 7-segment display

--- eod.c
+++ eod.c
@@ -1,1 +1,17 @@
+#include <stdint.h>
+	
+volatile uint32_t * LEDS = (uint32_t*)0XFF200020;
 
+
+const int segmentLookup[10] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
